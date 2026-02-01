811f531395bc4c451eebaec2eaab6acf300e6153
karunagutte <karuna.gutte@gmail.com>
2026-02-01 22:26:09 +0530

    bat file command changed

--- allure_run.bat
+++ allure_run.bat
@@ -1,1 +1,4 @@
-pytest -v -s -n=auto --browser edge --html=Html_Reports\my_report_31st_Jan_20206.html --alluredir="AllureReports"
+pytest -v -s -n=auto --browser edge --html=Html_Reports\my_report_02nd_February_20206.html --alluredir="AllureReports"
+
+
+
